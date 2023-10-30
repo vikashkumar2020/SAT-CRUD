57eca268cb3bc632d730428d144f29b5100715dc
Vikash Kumar <kvvik2020@gmail.com>
2023-10-31 01:34:38 +0530

    added the entity

--- internal/model/entity/results.go
+++ internal/model/entity/results.go
@@ -1,2 +1,17 @@
 package model
 
+import (
+    "gorm.io/gorm"
+)
+
+type URL struct {
+    gorm.Model 
+
+    Name     string `json:"name" gorm:"primaryKey;unique"`
+    Address  string `json:"address"`
+    City     string `json:"city"`
+    Country  string `json:"country"`
+    Pincode  string `json:"pincode"`
+    SatScore int    `json:"sat_score"`
+    Passed   string `json:"passed"`
+}
--- internal/routes/result.go
+++ internal/routes/result.go
@@ -5,5 +5,9 @@ import (
 )
 
 func RegisterResultRoutes(router *gin.RouterGroup) {
-
+	router.GET("/result", GetAllResult)
+	router.POST("/result", AddResult)
+	router.GET("/rank", GetRank)
+	router.PUT("/update", UpdateResult)
+	router.DELETE("/delete", DeleteResult)
 }
